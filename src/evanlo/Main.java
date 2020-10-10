d7faf6d540c21091f5536edc190b8576b83627d3
EvanLo427 <eggs.luo@gmail.com>
2020-10-10 16:00:35 -0600

    Initial commit

---
+++ src/evanlo/Main.java
@@ -0,0 +1,9 @@
+package evanlo;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String myName = "Evan";
+        System.out.println("Hello, my name is " + myName);
+    }
+}
